4802d1d2532724f1b902b12587d1533409ab29a2
KUSHAGRA SHUKLA <shukla.kushagra.business@gmail.com>
2023-04-16 19:41:37 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -21,6 +21,10 @@ mongoose.connect(
   }
 );
 
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname,"..","/client/build/index.html"));
+});
+
 app.listen(PORT, function() {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
