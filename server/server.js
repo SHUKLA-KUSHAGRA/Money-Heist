d2253b75629ee606dfa665d10e593d8b815e3543
KUSHAGRA SHUKLA <shukla.kushagra.business@gmail.com>
2023-04-16 20:53:33 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const cors = require("cors");
 const dotenv = require('dotenv');
 dotenv.config();
 const mongoose = require("mongoose");
@@ -9,6 +10,7 @@ const routes = require("./routes");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 app.use('/', routes);
 
 mongoose.connect(
